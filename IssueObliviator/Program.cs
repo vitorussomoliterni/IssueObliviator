006289126ac8a4c3537988547c17ff870d39a389
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-07-08 16:54:52 +1000

    Finished method to generate a list of older files.

--- IssueObliviator/Program.cs
+++ IssueObliviator/Program.cs
@@ -17,17 +17,20 @@ namespace IssueObliviator
 
         private static void MoveOlderFiles(List<Document> documents)
         {
-            var oldFiles = GetOldFilesList(documents);
+            var oldFiles = GetOldFilesList(documents).Distinct();
+            foreach (var f in oldFiles)
+            {
+                Console.WriteLine(f.FileName);
+            }
         }
 
         private static List<Document> GetOldFilesList(List<Document> documents)
         {
-            var filesToIgnore = documents.FindAll(f => f.IsRevisionCodeAcceptable() == false).ToList();
+            var filesToMove = new List<Document>();
 
-            foreach (var f in filesToIgnore)
-            {
-                documents.Remove(f);
-            }
+            var filesToIgnore = documents.FindAll(f => f.IsRevisionCodeAcceptable() == false).ToList();
+            
+            documents = RemoveFiles(documents, filesToIgnore);
 
             foreach (var doc in documents)
             {
@@ -40,24 +43,61 @@ namespace IssueObliviator
                     var codeGroupStatus = CheckCodeListContent(sameCodeGroup);
                     if (codeGroupStatus == "both")
                     {
-                        // Move all numbers
+                        var numberCodes = sameCodeGroup.FindAll(f => f.IsRevisionCodeANumber()).ToList();
+                        filesToMove.AddRange(numberCodes);
+                        sameCodeGroup = RemoveFiles(sameCodeGroup, numberCodes);
                     }
                     else if (codeGroupStatus == "numbers")
                     {
-                        // Get higher number and move everything else
+                        sameCodeGroup.Remove(sameCodeGroup.Last());
+                        filesToMove.AddRange(sameCodeGroup);
+                        break;
                     }
 
                     else if (codeGroupStatus == "letters")
                     {
-                        // Get latest file and move everything else
+                        sameCodeGroup = OrderLetters(sameCodeGroup);
+                        sameCodeGroup.Remove(sameCodeGroup.Last());
+                        filesToMove.AddRange(sameCodeGroup);
+                        break;
                     }
                 }
             }
 
-            return null;
+            return filesToMove;
         }
 
-        private static string CheckCodeListContent(List<Document> documents)
+        private static List<Document> OrderLetters(List<Document> list)
+        {
+            var lastElement = list.Last();
+
+            foreach (var f in list)
+            {
+                if (f.RevisionCode.Length >= lastElement.RevisionCode.Length)
+                {
+                    lastElement = f;
+                }
+            }
+
+            list.Remove(lastElement);
+            list.Add(lastElement);
+
+            return list;
+        }
+
+        private static List<Document> RemoveFiles(List<Document> files, List<Document> filesToRemove)
+        {
+            foreach (var f in filesToRemove)
+            {
+                files.Remove(f);
+            }
+
+            return files;
+        }
+
+
+
+        private static string CheckCodeListContent(List<Document> documents) // Analyses content of a code group
         {
             var CodesWithLetters = documents.FindAll(d => d.IsRevisionCodeANumber() == false).ToList();
             var CodesWithNumbers = documents.FindAll(d => d.IsRevisionCodeANumber() == true).ToList();
